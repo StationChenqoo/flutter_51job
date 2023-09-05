1e2259ccb3a88c67d35de1d605cc4f77827c7138
Station Chenqiao <i@cctv3.net>
2023-09-05 10:24:12 +0800

    💄 优化 GroupTitle 为 MyBoxBorder ...

--- lib/widgets/company-panel.dart
+++ lib/widgets/company-panel.dart
@@ -1,21 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_51job/models/Company.dart';
 import 'package:flutter_51job/widgets/common-card.dart';
+import 'package:flutter_51job/widgets/my-box-border.dart';
 
 class CompanyPanel extends StatelessWidget {
-  Widget buildGroupTitle(context, title, icon) => Container(
-        child: Row(
-          children: [
-            Text(
-              title,
-              style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500),
-            )
-          ],
-        ),
-      );
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -48,107 +36,124 @@ class CompanyPanel extends StatelessWidget {
                           ],
                         ),
                         SizedBox(
-                          height: 5,
+                          height: 20,
                         ),
-                        buildGroupTitle(context, '-- 线上项目 --', ''),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        ...e.value.link
-                            .map((e) => Container(
-                                  margin: EdgeInsets.only(bottom: 4),
-                                  child: Row(
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Image.asset(
-                                            e.icon,
-                                            height: 16,
-                                            width: 16,
-                                            color:
-                                                Theme.of(context).primaryColor,
-                                          )
-                                        ],
-                                      ),
-                                      SizedBox(
-                                        width: 4,
-                                      ),
-                                      Text(
-                                        e.name,
-                                        style: TextStyle(
-                                            color:
-                                                Theme.of(context).primaryColor,
-                                            fontSize: 14),
-                                      )
-                                    ],
-                                  ),
-                                ))
-                            .toList(),
-                        SizedBox(
-                          height: 12,
-                        ),
-                        buildGroupTitle(context, '-- 主要贡献 --', ''),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        ...e.value.honor
-                            .map((e) => Container(
-                                margin: EdgeInsets.only(bottom: 4),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      e.name,
-                                      style: TextStyle(
-                                          fontSize: 14, color: Colors.black),
-                                    )
-                                  ],
-                                )))
-                            .toList(),
+                        MyBoxBorder(
+                            title: '线上项目',
+                            padding: EdgeInsets.only(
+                                left: 12, right: 12, bottom: 8, top: 16),
+                            child: Column(
+                              children: [
+                                ...e.value.link
+                                    .map((e) => Container(
+                                          margin: EdgeInsets.only(bottom: 4),
+                                          child: Row(
+                                            children: [
+                                              Row(
+                                                children: [
+                                                  Image.asset(
+                                                    e.icon,
+                                                    height: 16,
+                                                    width: 16,
+                                                    color: Theme.of(context)
+                                                        .primaryColor,
+                                                  )
+                                                ],
+                                              ),
+                                              SizedBox(
+                                                width: 4,
+                                              ),
+                                              Text(
+                                                e.name,
+                                                style: TextStyle(
+                                                    color: Theme.of(context)
+                                                        .primaryColor,
+                                                    fontSize: 14),
+                                              )
+                                            ],
+                                          ),
+                                        ))
+                                    .toList(),
+                              ],
+                            )),
                         SizedBox(
                           height: 12,
                         ),
-                        buildGroupTitle(context, '-- 工作内容 --', ''),
+                        MyBoxBorder(
+                            title: '主要贡献',
+                            padding: EdgeInsets.only(
+                                left: 12, right: 12, bottom: 8, top: 16),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ...e.value.honor
+                                    .map((e) => Container(
+                                        margin: EdgeInsets.only(bottom: 4),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              e.name,
+                                              style: TextStyle(
+                                                  fontSize: 14,
+                                                  color: Colors.black),
+                                            )
+                                          ],
+                                        )))
+                                    .toList(),
+                              ],
+                            )),
                         SizedBox(
-                          height: 5,
+                          height: 12,
                         ),
-                        ...e.value.detail
-                            .map((e) => Container(
-                                margin: EdgeInsets.only(bottom: 4),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      e.name,
-                                      style: TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.w500),
-                                    ),
-                                    SizedBox(
-                                      height: 5,
-                                    ),
-                                    Container(
-                                      margin: EdgeInsets.only(left: 20),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          ...(e.value as List<String>)
-                                              .map((e) => Container(
-                                                    child: Text(
-                                                      ' · ${e}',
-                                                      style: TextStyle(
-                                                          fontSize: 14),
-                                                    ),
-                                                  ))
-                                              .toList()
-                                        ],
-                                      ),
-                                    )
-                                  ],
-                                )))
-                            .toList(),
+                        MyBoxBorder(
+                            title: '工作内容',
+                            padding: EdgeInsets.only(
+                                left: 12, right: 12, bottom: 8, top: 16),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ...e.value.detail
+                                    .map((e) => Container(
+                                        margin: EdgeInsets.only(bottom: 4),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              e.name,
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  color: Colors.black,
+                                                  fontWeight: FontWeight.w500),
+                                            ),
+                                            SizedBox(
+                                              height: 5,
+                                            ),
+                                            Container(
+                                              margin: EdgeInsets.only(left: 20),
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  ...(e.value as List<String>)
+                                                      .map((e) => Container(
+                                                            child: Text(
+                                                              ' · ${e}',
+                                                              style: TextStyle(
+                                                                  fontSize: 14),
+                                                            ),
+                                                          ))
+                                                      .toList()
+                                                ],
+                                              ),
+                                            )
+                                          ],
+                                        )))
+                                    .toList(),
+                              ],
+                            ))
                       ])),
                 ))
             .toList(),
